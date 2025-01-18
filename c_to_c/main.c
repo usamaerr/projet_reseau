d2c75f60c6865137a76686d6b30ca2cc946c12ae
usamaerr <erraqiq.oussama13@gmail.com>
2025-01-18 12:04:05 +0100

    ok
    ok

--- c_to_c/main.c
+++ c_to_c/main.c
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+
+#define BUFFER_SIZE 1024
 
 // Helper function to set a socket to non-blocking mode
 void set_nonblocking(int sock) {
@@ -14,65 +17,130 @@ void set_nonblocking(int sock) {
 }
 
 int main() {
-    int recv_port, send_port;
-    char buffer[1024];
-    struct sockaddr_in recv_addr, relay_addr, sender_addr;
+    int recv_port_python, send_port_python, recv_port_c, send_port_c;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in recv_addr_python, send_addr_python, recv_addr_c, send_addr_c, sender_addr;
     socklen_t addr_len = sizeof(struct sockaddr_in);
 
-    // Ports pour recevoir et relayer
-    printf("Port pour recevoir des messages (du processus Python) : ");
-    scanf("%d", &recv_port);
-    printf("Port pour relayer les messages (au second processus) : ");
-    scanf("%d", &send_port);
+    // Demande des ports à l'utilisateur
+    printf("Port pour recevoir des messages de Python : ");
+    scanf("%d", &recv_port_python);
+    printf("Port pour envoyer des messages à Python : ");
+    scanf("%d", &send_port_python);
+    printf("Port pour recevoir des messages du programme C : ");
+    scanf("%d", &recv_port_c);
+    printf("Port pour envoyer des messages au programme C : ");
+    scanf("%d", &send_port_c);
 
-    // Création du socket
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("Échec de la création du socket");
+    // Création des sockets
+    int sock_python = socket(AF_INET, SOCK_DGRAM, 0);
+    int sock_c = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sock_python < 0 || sock_c < 0) {
+        perror("Erreur lors de la création des sockets");
         exit(EXIT_FAILURE);
     }
 
-    // Configuration pour recevoir sur recv_port
-    memset(&recv_addr, 0, sizeof(recv_addr));
-    recv_addr.sin_family = AF_INET;
-    recv_addr.sin_port = htons(recv_port);
-    recv_addr.sin_addr.s_addr = INADDR_ANY;
+    // Configuration de l'adresse pour recevoir de Python
+    memset(&recv_addr_python, 0, sizeof(recv_addr_python));
+    recv_addr_python.sin_family = AF_INET;
+    recv_addr_python.sin_port = htons(recv_port_python);
+    recv_addr_python.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sock, (struct sockaddr *)&recv_addr, sizeof(recv_addr)) < 0) {
-        perror("Échec du binding");
-        close(sock);
+    if (bind(sock_python, (struct sockaddr *)&recv_addr_python, sizeof(recv_addr_python)) < 0) {
+        perror("Erreur de binding pour Python");
+        close(sock_python);
         exit(EXIT_FAILURE);
     }
 
-    // Adresse pour relayer les messages
-    memset(&relay_addr, 0, sizeof(relay_addr));
-    relay_addr.sin_family = AF_INET;
-    relay_addr.sin_port = htons(send_port);
-    relay_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Remplacez par l'adresse réelle si nécessaire
+    // Configuration de l'adresse pour envoyer à Python
+    memset(&send_addr_python, 0, sizeof(send_addr_python));
+    send_addr_python.sin_family = AF_INET;
+    send_addr_python.sin_port = htons(send_port_python);
+    send_addr_python.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    set_nonblocking(sock);
+    // Configuration de l'adresse pour recevoir de C
+    memset(&recv_addr_c, 0, sizeof(recv_addr_c));
+    recv_addr_c.sin_family = AF_INET;
+    recv_addr_c.sin_port = htons(recv_port_c);
+    recv_addr_c.sin_addr.s_addr = INADDR_ANY;
 
-    printf("En écoute sur le port %d pour relayer vers le port %d.\n", recv_port, send_port);
+    if (bind(sock_c, (struct sockaddr *)&recv_addr_c, sizeof(recv_addr_c)) < 0) {
+        perror("Erreur de binding pour C");
+        close(sock_c);
+        exit(EXIT_FAILURE);
+    }
 
+    // Configuration de l'adresse pour envoyer à C
+    memset(&send_addr_c, 0, sizeof(send_addr_c));
+    send_addr_c.sin_family = AF_INET;
+    send_addr_c.sin_port = htons(send_port_c);
+    send_addr_c.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Mettre les sockets en mode non bloquant
+    set_nonblocking(sock_python);
+    set_nonblocking(sock_c);
+
+    printf("En écoute sur les ports suivants :\n");
+    printf("  Python -> Recevoir : %d, Envoyer : %d\n", recv_port_python, send_port_python);
+    printf("  C      -> Recevoir : %d, Envoyer : %d\n", recv_port_c, send_port_c);
+
+    // Boucle principale avec `select()`
     while (1) {
-        // Réception non bloquante
-        ssize_t recv_len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&sender_addr, &addr_len);
-        if (recv_len > 0) {
-            buffer[recv_len] = '\0'; // Ajouter un terminateur à la chaîne
-            printf("Message reçu : %s\n", buffer);
+        fd_set readfds;
+        FD_ZERO(&readfds);
 
-            // Relayer le message au second processus
-            ssize_t send_len = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&relay_addr, addr_len);
-            if (send_len < 0) {
-                perror("Échec de l'envoi du message");
-            } else {
-                printf("Message relayé : %s\n", buffer);
+        // Ajouter les sockets au set
+        FD_SET(sock_python, &readfds);
+        FD_SET(sock_c, &readfds);
+
+        int max_fd = (sock_python > sock_c) ? sock_python : sock_c;
+
+        // Attendre une activité sur une des sockets
+        int activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+
+        if (activity < 0) {
+            perror("Erreur avec select()");
+            continue;
+        }
+
+        // Vérifier si une activité est présente sur la socket Python
+        if (FD_ISSET(sock_python, &readfds)) {
+            ssize_t recv_len = recvfrom(sock_python, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr *)&sender_addr, &addr_len);
+            if (recv_len > 0) {
+                buffer[recv_len] = '\0';
+                printf("[Python] Message reçu : %s\n", buffer);
+
+                // Envoyer le message au programme C
+                ssize_t send_len = sendto(sock_c, buffer, strlen(buffer), 0, (struct sockaddr *)&send_addr_c, addr_len);
+                if (send_len < 0) {
+                    perror("Erreur d'envoi vers C");
+                } else {
+                    printf("[Python -> C] Message relayé : %s\n", buffer);
+                }
             }
         }
 
-        usleep(100000); // Éviter le busy-waiting
+        // Vérifier si une activité est présente sur la socket C
+        if (FD_ISSET(sock_c, &readfds)) {
+            ssize_t recv_len = recvfrom(sock_c, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr *)&sender_addr, &addr_len);
+            if (recv_len > 0) {
+                buffer[recv_len] = '\0';
+                printf("[C] Message reçu : %s\n", buffer);
+
+                // Envoyer le message au programme Python
+                ssize_t send_len = sendto(sock_python, buffer, strlen(buffer), 0, (struct sockaddr *)&send_addr_python, addr_len);
+                if (send_len < 0) {
+                    perror("Erreur d'envoi vers Python");
+                } else {
+                    printf("[C -> Python] Message relayé : %s\n", buffer);
+                }
+            }
+        }
     }
 
-    close(sock);
+    close(sock_python);
+    close(sock_c);
+
     return 0;
 }
